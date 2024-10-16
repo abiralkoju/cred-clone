170d40093b69d6ae3ed52b897c790983b88e49b4
Abiral koju <abiral.koju@gmail.com>
2024-10-16 21:52:37 +0545

    debugging header

--- clone/src/components/common/Header/index.jsx
+++ clone/src/components/common/Header/index.jsx
@@ -25,12 +25,14 @@ const Header = () => {
         />
         <div className="only-mobile mobile-menu-button-wrapper">
           <button
-            class={`hamburger hamburger-spin ${showMobMenu ? "is-active" : ""}`}
+            className={`hamburger hamburger-spin ${
+              showMobMenu ? "is-active" : ""
+            }`}
             type="button"
             onClick={toggleMobileMenu}
           >
-            <span class="hamburger-box">
-              <span class="hamburger-inner"></span>
+            <span className="hamburger-box">
+              <span className="hamburger-inner"></span>
             </span>
           </button>
         </div>
